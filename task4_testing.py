01e15a7c94d2f9f6face429d35ddfb6d685c6bd8
Ming Yi <myi@student.ethz.ch>
2019-12-05 19:47:30 +0100

    updated

--- task4_testing.py
+++ task4_testing.py
@@ -5,16 +5,22 @@ import pandas as pd
 
 def test_eeg(eeg1, eeg2):
     # testing plot
-    # remove nan value in nparray
     x_sample = np.concatenate((eeg1, eeg2), axis = 0)
-    x_sample = np.asarray(x_sample)
     # transpose to put the signals into column
     x_sample = np.transpose(x_sample)
-    print(x_sample)
+    # print theta
+    # print(x_sample)
 
     # x_sample = np.concatenate((x_sample, x_sample), axis=0)
-    signal_processed = eeg.eeg(signal=x_sample, sampling_rate=128, show=True)
-
+    signal_processed = eeg.eeg(signal=x_sample, sampling_rate=128, show=False)
+    # # theta
+    theta = signal_processed[3]
+    alow = signal_processed[4]
+    ahigh = signal_processed[5]
+    beta = signal_processed[6]
+    gamma = signal_processed[7]
+    features = np.concatenate((theta, alow, ahigh, beta, gamma), axis=0).ravel()
+    print(features.shape)
 
 def read_from_file(eeg1, eeg2, nrows = 10):
     # read from files
@@ -24,11 +30,14 @@ def read_from_file(eeg1, eeg2, nrows = 10):
 
 
 if __name__ == '__main__':
-    train_part = read_from_file("train_eeg1.csv", "train_eeg2.csv", 5)
+    train_part = read_from_file("train_eeg1.csv", "train_eeg2.csv", 4)
     eeg1s = train_part[0]
     eeg2s = train_part[1]
-
-    eeg1 = eeg1s[1, :].reshape(1, -1)
-    eeg2 = eeg1s[1, :].reshape(1, -1)
-    print(eeg1.shape) # size 1 * 512
+    # for mat in zip(eeg1s, eeg2s):
+    #     print(mat)
+    #     print(mat[0])
+    #     break
+    eeg1 = eeg1s[3, :].reshape(1, -1)
+    eeg2 = eeg1s[3, :].reshape(1, -1)
+    # print(eeg1.shape) # size 1 * 512
     test_eeg(eeg1, eeg2)
